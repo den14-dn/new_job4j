5b703d2db3dd7947575b9f973696298ac36e6c4d
Denis Shubko <den14-dn@yandex.ru>
2020-01-17 11:33:09 +0300

    3. Реализация профессий в коде [#6837]

--- chapter_002/src/main/java/ru/job4j/profession/Builder.java
+++ chapter_002/src/main/java/ru/job4j/profession/Builder.java
@@ -3,6 +3,6 @@ package ru.job4j.profession;
 public class Builder extends Engineer {
     private String level;
     public String getLevel() {
-
+        return "ok";
     }
 }
--- chapter_002/src/main/java/ru/job4j/profession/Therapist.java
+++ chapter_002/src/main/java/ru/job4j/profession/Therapist.java
@@ -3,6 +3,6 @@ package ru.job4j.profession;
 public class Therapist extends Doctor{
     private String Classiness;
     public String getClassiness() {
-
+        return "ok";
     }
 }
