8f70366d405788b2057ec51d8cf4fdd1ea0dd76b
Denis Shubko <den14-dn@yandex.ru>
2020-01-18 21:43:06 +0300

    5. Реализовать класс Tracker [#396]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,75 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
+    /**
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод добавления заявки в хранилище
+     * @param item новая заявка
+     * @return irem
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        this.items[position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод возвращает из хранилища все созданные заявки
+     * @return массив item'ов
+     */
+    public Item[] findAll() {
+        return Arrays.copyOf(this.items, position);
+    }
+
+    /**
+     * Метод для поиска всех заявок по наименованию
+     * @param key для поиска по наименованию
+     * @return массив item'ов
+     */
+    public Item[] findByName(String key) {
+        Item[] copyItems = Arrays.copyOf(this.items, this.items.length);
+        int index = 0;
+        for (Item el : this.items) {
+            if (el != null && el.getName().equals(key)) {
+                copyItems[index++] = el;
+            }
+        }
+        return Arrays.copyOf(copyItems, index);
+    }
+
+    /**
+     * Метод поиска заявки по id.
+     * @param id
+     * @return item
+     */
+    public Item findById(String id) {
+        for (Item el : this.items) {
+            if (el.getId().equals(id)) {
+                return el;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описания. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
 }
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+    @Test
+    public void whenAddNewItemAndFindAllThenReturnOneItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item[] result = tracker.findAll();
+        assertThat(result.length, is(1));
+    }
+    @Test
+    public void whenAddNewItemAndFindByNameThenHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item[] result = tracker.findByName(item.getName());
+        assertThat(result.length, is(1));
+    }
+}
