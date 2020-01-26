c2f6dfec69a8410dc8ca7f161601d49b25b49378
Denis Shubko <den14-dn@yandex.ru>
2020-01-26 23:28:08 +0300

    9.2. Тесты вывода на консоль в StartUI [#33585]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/FindByNameActionTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class FindByNameActionTest {
+    @Test
+    public void whenFindByName() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+
+        Item item = new Item("fix bug");
+        Tracker tracker = new Tracker();
+        tracker.add(item);
+        StubInput input = new StubInput(new String[] {"fix bug"});
+        new FindByNameAction().execute(input, tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Item: " + item.getName() + ", id: " + item.getId())
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+
+        System.setOut(def);
+    }
+}
--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,7 +1,9 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
-
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
 
@@ -12,5 +14,23 @@ public class StartUITest {
         StubAction action = new StubAction();
         new StartUI().init(input, new Tracker(), new UserAction[] { action });
         assertThat(action.isCall(), is(true));
+    }
+
+    @Test
+    public void whenPrtMenu() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+
+        StubInput input = new StubInput(new String[] {"0"});
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Menu.")
+                .add("0. Stub action").toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+
+        System.setOut(def);
     }
 }
