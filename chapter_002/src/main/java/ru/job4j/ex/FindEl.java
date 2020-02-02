1c2e50765859cc9c366f2367d3556d26f99c0bc7
Denis Shubko <den14-dn@yandex.ru>
2020-02-02 15:16:15 +0300

    0.4. Пользовательские исключения. [#219367]

--- chapter_002/src/main/java/ru/job4j/ex/FindEl.java
+++ chapter_002/src/main/java/ru/job4j/ex/FindEl.java
@@ -1,7 +1,7 @@
 package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) throws UserInputException{
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rst = -1;
         for (int index = 0; index < value.length; index++) {
             if (value[index] == key) {
@@ -10,16 +10,16 @@ public class FindEl {
         }
 
         if (rst == -1) {
-            throw new UserInputException(key + " not found in array");
+            throw new ElementNotFoundException(key + " not found in array");
         }
         return rst;
     }
     public static void main(String[] args) {
         String[] arrayStrings = {"one", "two", "three", "four"};
         try {
-            int index = indexOf(arrayStrings, "twoo");
+            int index = indexOf(arrayStrings, "two");
             System.out.println("Element found under index " + index);
-        } catch (UserInputException e) {
+        } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
 
