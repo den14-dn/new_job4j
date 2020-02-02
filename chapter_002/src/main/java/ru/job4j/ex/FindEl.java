01abbf247de5b429b0aeb8b9a0c2f9de5b42bd03
Denis Shubko <den14-dn@yandex.ru>
2020-02-02 20:08:02 +0300

    0.5. Иерархия исключений и множественный catch. [#219368]

--- chapter_002/src/main/java/ru/job4j/ex/FindEl.java
+++ chapter_002/src/main/java/ru/job4j/ex/FindEl.java
@@ -1,10 +1,10 @@
 package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+    public static int indexOf(String[] values, String key) throws ElementNotFoundException {
         int rst = -1;
-        for (int index = 0; index < value.length; index++) {
-            if (value[index].equals(key)) {
+        for (int index = 0; index < values.length; index++) {
+            if (values[index].equals(key)) {
                 rst = index;
                 break;
             }
@@ -15,14 +15,37 @@ public class FindEl {
         }
         return rst;
     }
+
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        for (String el : abuses) {
+            if (el.equals(value)) {
+                throw new ElementAbuseException(value + " is in the list abuses");
+            }
+        }
+        return true;
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
+        try {
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+//        } catch (ElementAbuseException ea) {
+//            ea.printStackTrace();
+//        } catch (ElementNotFoundException en) {
+//            en.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+//        } catch (Throwable th) {
+//            th.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         String[] arrayStrings = {"one", "two", "three", "four"};
-        try {
-            int index = indexOf(arrayStrings, "two");
-            System.out.println("Element found under index " + index);
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
-        }
+        String key = "two";
+        String[] abuses = {"one", "five", "seven"};
 
+        process(arrayStrings, key, abuses);
     }
 }
---
+++ chapter_002/src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,38 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        for (User el : users) {
+            if (el.getUsername().equals(login)) {
+                return el;
+            }
+        }
+        throw new UserNotFoundException("User " + login + " not found in user list");
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (user.isValid() == false || user.getUsername().length() < 3) {
+            throw new UserInvalidException(user + " not valid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {new User("Denis Shubko", true)};
+
+        try {
+            User user = findUser(users, "Denis Shubko");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException uie) {
+            uie.printStackTrace();
+        } catch (UserNotFoundException une) {
+            une.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
