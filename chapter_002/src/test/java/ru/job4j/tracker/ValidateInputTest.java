f54a022ab1bd71bbbb9b0d1526c1630a89e2440f
Denis Shubko <den14-dn@yandex.ru>
2020-02-09 22:20:30 +0300

    1.1. Тесты на ValidateInput. [#223313]

--- chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -7,7 +7,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class ValidateInputTest {
-
     @Test
     public void whenInvalidInput() {
         ByteArrayOutputStream mem = new ByteArrayOutputStream();
@@ -16,10 +15,26 @@ public class ValidateInputTest {
 
         String[] data = {"one", "1"};
         ValidateInput input = new ValidateInput(new StubInput(data));
-        input.askInt("Enter");
+        input.askInt("Enter", 1);
         assertThat(
                 mem.toString(),
                 is(String.format("Please select validate data again.%n"))
+        );
+
+        System.setOut(out);
+    }
+    @Test
+    public void whenInvalidSelectKey() {
+        ByteArrayOutputStream mem = new ByteArrayOutputStream();
+        PrintStream out = System.out;
+        System.setOut(new PrintStream(mem));
+
+        String[] data = {"7", "1"};
+        ValidateInput input = new ValidateInput(new StubInput(data));
+        input.askInt("Enter", 6);
+        assertThat(
+                mem.toString(),
+                is(String.format("Please select key from menu.%n"))
         );
 
         System.setOut(out);
