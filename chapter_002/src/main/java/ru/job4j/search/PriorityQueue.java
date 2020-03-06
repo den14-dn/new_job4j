404414d4526a8f0bcc967124d5f02338347053c4
Denis Shubko <den14-dn@yandex.ru>
2020-03-06 23:43:27 +0300

    2. Очередь с приоритетом на LinkedList [#41670]

---
+++ chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,23 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        int priorityTask = task.getPriority();
+        int index = 0;
+        for (Task element : tasks) {
+            if (element.getPriority() > priorityTask) {
+                break;
+            }
+            index++;
+        }
+        this.tasks.add(index, task);
+    }
+
+    public Task take() {
+        return tasks.remove(0);
+    }
+}
