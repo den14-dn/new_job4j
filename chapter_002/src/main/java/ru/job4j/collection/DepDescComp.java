050fba2fb3832b6b9776e5d3a3975b4b02c2f3cc
den14-dn <den14-dn@yandex.ru>
2020-07-02 21:41:47 +0300

    Отсортировать департаменты [#34352]

--- chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_002/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -9,26 +9,7 @@ public class DepDescComp implements Comparator<String> {
         String[] arResultO1 = o1.split("/");
         String[] arResultO2 = o2.split("/");
 
-        int size = Math.min(arResultO1.length, arResultO2.length);
-        int rst = arResultO1.length - arResultO2.length;
-
-        if (size > 0) {
-            int rstFirst = arResultO2[0].compareTo(arResultO1[0]);
-            if (rstFirst == 0) {
-                if (rst != 0) {
-                    return rst;
-                }
-                for (int index = 1; index < size; index++) {
-                    rst = arResultO1[index].compareTo(arResultO2[index]);
-                    if (rst != 0) {
-                        return rst;
-                    }
-                }
-            } else {
-                return rstFirst;
-            }
-        }
-
-        return rst;
+        int rst = arResultO2[0].compareTo(arResultO1[0]);
+        return rst == 0 ? o1.compareTo(o2) : rst;
     }
 }
