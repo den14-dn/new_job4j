5b7215df8ffbff8610349229178369400e5cd11e
Denis Shubko <den14-dn@yandex.ru>
2020-08-02 01:21:19 +0300

    2.2. Лямбда блок [#249208]

--- chapter_002/src/main/java/ru/job4j/lambda/Lesson421.java
+++ chapter_002/src/main/java/ru/job4j/lambda/Lesson421.java
@@ -4,5 +4,8 @@ import java.util.Comparator;
 
 public class Lesson421 {
     Comparator<String> cmpText = (left, right) -> left.compareTo(right);
-    Comparator<String> cmpDescSize = (left, right) -> right.length() - left.length();
+    Comparator<String> cmpDescSize = (left, right) -> {
+        System.out.println("compare : " + right.length() + " - " + left.length());
+        return right.length() - left.length();
+    };
 }
